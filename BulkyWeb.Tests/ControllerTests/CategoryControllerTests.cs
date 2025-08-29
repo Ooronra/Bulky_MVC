1c56b34460ce2e4088aaee24bfab13c391eb6afe
Oronnra <arda.kanat39@gmail.com>
2025-08-29 13:05:18 +0300

    Created BulkyWeb.Tests project. Implemented test for Index action of CategoryController in CategoryControllerTests.

---
+++ BulkyWeb.Tests/ControllerTests/CategoryControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using BulkyWeb.Areas.Admin.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyWeb.Tests.ControllerTests
+{
+    public class CategoryControllerTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly Mock<ICategoryRepository> _categoryRepositoryMock;
+
+
+        public CategoryControllerTests()
+        {
+            //Dependencies
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _categoryRepositoryMock = new Mock<ICategoryRepository>();
+
+        }
+
+        [Fact]
+        public void CategoryController_Index_ReturnsViewResultWithCategoryList()
+        {
+            // Arrange
+            
+            var categories = new List<Category>
+            {
+                new Category { Id = 1, Name = "Drama", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Horror", DisplayOrder = 2 }
+            };
+
+            _categoryRepositoryMock.Setup(repo => repo.GetAll(It.IsAny<string?>())).Returns(categories);
+            _unitOfWorkMock.Setup(uow => uow.Category).Returns(_categoryRepositoryMock.Object);
+
+            var controller = new CategoryController(_unitOfWorkMock.Object); // SUT (System Under Test) created here
+
+            // Act
+            
+            var result = controller.Index();
+
+            // Assert
+
+            result.Should().BeOfType<ViewResult>();
+            var viewResult = result as ViewResult;
+            viewResult!.Model.Should().BeEquivalentTo(categories);
+
+        }
+    }
+}
---
+++ BulkyWeb.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BulkyWeb.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
